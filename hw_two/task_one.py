345750da7ffedc258fbc39ca62902ca3f8611fec
letusbeus <letusbeus@mail.ru>
2023-04-09 22:56:01 +0300

    Homework two, updated task 1, added task 2

--- hw_two/task_one.py
+++ hw_two/task_one.py
@@ -1,18 +1,16 @@
-"""
-Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое представление.
-Функцию hex используйте для проверки своего результата.
-"""
+import view
 
 
 def check_user_input():
-    welcome_message()
+    view.task_one()
+    view.task_one_welcome_message()
     while True:
         user_number = input()
         if user_number.isdigit():
             user_number = int(user_number)
             return user_number
         else:
-            warning_message()
+            view.task_one_warning_message()
 
 
 def convert_to_hex():
@@ -23,24 +21,8 @@ def convert_to_hex():
         hex_string += "0123456789abcdef"[num_to_be_converted % 16]
         num_to_be_converted //= 16
         if num_to_be_converted == 0:
-            convert_confirmation()
+            view.conversion_confirmation()
             print("0x" + hex_string[::-1])
             break
-    built_in_conversion()
-    print(hex(num_to_check))
-
-
-def warning_message():
-    print('Only integers are allowed, please check your input and try again: ')
-
-
-def welcome_message():
-    print('Please enter a number to convert to hexadecimal: ')
-
-
-def convert_confirmation():
-    print('The result of the number conversion: ', end='')
-
-
-def built_in_conversion():
-    print('Conversion using the built-in function: ', end='')
+    view.built_in_conversion()
+    print(hex(num_to_check), end='\n\n')

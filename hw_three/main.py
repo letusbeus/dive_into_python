314b4dc1f01d85bb18ed411ea181c267c08bb83b
letusbeus <letusbeus@mail.ru>
2023-04-12 20:59:41 +0300

    Homework three, tasks 1-2

---
+++ hw_three/main.py
@@ -0,0 +1,8 @@
+import task_one
+import task_two
+import task_three
+from user_data import article
+
+task_one.duplicate_list([1, 4, 5, 7, 8, 3, 4, 6, 8, 9, 2, 2, 34, 6, 8, 8])
+task_one.duplicate_list(['qwe', 'wer', 'qwe', 've', 'sdf', 'jf', 'qwe', 'wer'])
+task_two.print_most_common(article)
---
+++ hw_three/task_two.py
@@ -0,0 +1,14 @@
+import collections
+import view
+
+
+def print_most_common(given_str):
+    view.task_two()
+    text_dict = dict(collections.Counter(list(given_str.split(' '))).most_common(10))
+    view.most_common()
+    print_dict(text_dict)
+
+
+def print_dict(given_dict):
+    for key, value in given_dict.items():
+        print("{0}: {1} ".format(key, value))

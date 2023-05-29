49f07aacaa71338cf596a282519917726d24ac38
letusbeus <letusbeus@mail.ru>
2023-05-29 19:35:22 +0300

    Homework 10, task 2: done. Working on improvements.

---
+++ hw_ten/atm.py
@@ -0,0 +1,146 @@
+import view
+
+class atm:
+    bank_account = 0
+    count = 0
+    start = 0
+    operations = []
+
+    def __init__(self, money: int | float = 0):
+        self.money = money
+
+    def operation_logs(self, val: int | float):
+        self.operations.append(val)
+    def add_money(self, money: int | float):
+        while not money % 50 == 0:
+            view.multiplicity_message()
+            money = int(input())
+        self.operation_logs(self.bank_account + money)
+        self.bank_account += money
+
+    def get_money(self, money: int | float):
+        while money % 50 != 0:
+            view.multiplicity_message()
+            money = int(input())
+        if money <= 2_000:
+            self.bank_account -= (money + 30)
+            self.operation_logs(-self.bank_account)
+        elif 2_001 <= money <= 40_000:
+            self.bank_account -= money * 1.015
+            self.operation_logs(-self.bank_account)
+        else:
+            self.bank_account -= (money + 600)
+            self.operation_logs(self.bank_account)
+
+    # Выход из программы
+    @staticmethod
+    def bank_exit():
+        return view.goodbye()
+
+    # Распечатать баланс счета
+    def print_bank(self):
+        """
+        Выводит информацию о счете в банкомате
+        :return: str
+        """
+        return f'На вашем счете: {self.bank_account} руб.'
+
+    # Проверка и списание налога на богатство (10%)
+    def check_bank(self):
+        """
+        Проверка налога на богатство при сумме на счете более 5 млн. (10%)
+        :return: None
+        """
+        if self.bank_account >= 5_000_000:
+            self.bank_account = self.bank_account * 0.90
+            return self.bank_account, f'С Вас вычли 10% налога на богатство, баланс счета: {self.bank_account}'
+        else:
+            return self.bank_account, None
+
+    # Проверка корректности ввода
+    def check_input_value(self, val: str, flag=0) -> int:
+        """
+        Проверка корректности ввода данных
+        :param val: int|float
+        :param flag: int
+        :return: int
+        """
+        while isinstance(val, (int | float)):
+            val = input('Банкомат принимает только Целые числа и числа с плавающей точкой, введите снова!\n'
+                        '|--->: ')
+        if flag == 1:
+            while self.bank_account - int(val) < 0:
+                val = input(f'У вас недостаточно денег для снятия! в банке {self.bank_account}!\n'
+                            '|--->: ')
+        return int(val)
+
+    # Счетчик операций и начисление процентов на остаток
+    def count_checker(self, count_cur):
+        """
+        Счетчик операций и начисление процентов на остаток
+        :param count_cur: int
+        :return: None
+        """
+        if count_cur < 2:
+            self.count += 1
+            return self.count, self.bank_account
+        if count_cur >= 2:
+            self.count = 0
+            self.bank_account = self.bank_account * 1.03
+            return self.bank_account, self.bank_account, f'Начислены проценты!) Баланс счета: {self.bank_account}'
+
+    def show_log(self):
+        """
+        Выводит лог операций по счету
+        :return:
+        """
+        info = 'Operations performed in the current session:\n'
+        for operation in self.operations:
+            if operation > 0:
+                info += f'Enrollment {operation} y.e.\n'
+            elif operation < 0:
+                info += f'Withdrawal {operation} y.e.'
+            else:
+                continue
+        return info
+
+    # match/case пользовательского ввода
+    @staticmethod
+    def user_choice(value: int):
+        """
+        'match/case' - выборы пользователя
+        :param value: int
+        :return: str
+        """
+        match value:
+            case 1:
+                return 'Пополнить счет'
+            case 2:
+                return 'Снять деньги со счета'
+            case 3:
+                return 'Проверить счет'
+            case 4:
+                return 'Выйти'
+            case 5:
+                return 'Посмотреть Лог операций'
+
+    # match/case работы программы ввода
+    def start_bank(self, value: str):
+        """
+        'match/case' - пользовательских выборов
+        :param value: str
+        :return: int|float
+        """
+        match value:
+            case 'Пополнить счет':
+                money = self.check_input_value(input('Введите сумму для пополнения: '))
+                return self.add_money(money)
+            case 'Снять деньги со счета':
+                money = self.check_input_value(input('Введите сумму для снятия: '), 1)
+                return self.get_money(money)
+            case 'Проверить счет':
+                return self.print_bank()
+            case 'Выйти':
+                return self.bank_exit()
+            case 'Посмотреть Лог операций':
+                return self.show_log()
--- hw_ten/main.py
+++ hw_ten/main.py
@@ -1,16 +1,46 @@
-from view import task_animal_farm
+from atm import atm
+from view import task_animal_farm, task_cashier, task_cashier_desc
 from animals import Animal, Bird, Dog, Cat, Fish, Horse
 from animal_farm import AnimalFarm
 
 if __name__ == "__main__":
-    task_animal_farm()
-    vanda = AnimalFarm.create_animal(Fish, name='Vanda', age=5, color='Rainbow')
-    carl = AnimalFarm.create_animal(Bird, name='Carl', age=8, color='Black', voice='CRAW!')
-    fido = AnimalFarm.create_animal(Dog, name='Fido', age=5, voice='Woof!', fur='Pale, long')
-    tom = AnimalFarm.create_animal(Cat, name='Tom', age=2, fur='brown', voice='meow!')
-    starlight = AnimalFarm.create_animal(Horse, name='starlight', age=7, hair='blond', voice='Ghrrr')
-    print(vanda.get_info())
-    print(fido.get_info())
-    print(tom.get_info())
-    print(starlight.get_info())
-    print(carl.get_info())
+    # task_animal_farm()
+    # vanda = AnimalFarm.create_animal(Fish, name='Vanda', age=5, color='Rainbow')
+    # carl = AnimalFarm.create_animal(Bird, name='Carl', age=8, color='Black', voice='CRAW!')
+    # fido = AnimalFarm.create_animal(Dog, name='Fido', age=5, voice='Woof!', fur='Pale, long')
+    # tom = AnimalFarm.create_animal(Cat, name='Tom', age=2, fur='brown', voice='meow!')
+    # starlight = AnimalFarm.create_animal(Horse, name='starlight', age=7, hair='blond', voice='Ghrrr')
+    # print(vanda.get_info())
+    # print(fido.get_info())
+    # print(tom.get_info())
+    # print(starlight.get_info())
+    # print(carl.get_info())
+
+    task_cashier()
+    task_cashier_desc()
+    my_bank = atm(0)
+
+    while True:
+        start = int(input(f'{"Добро пожаловать в банк"}\n'
+                          f'{"Вы в меню"}\n'
+                          f'{"Введите цифру желаемой услуги"}\n'
+                          f'1 - Пополнить счет\n'
+                          f'2 - Снять деньги со счета\n'
+                          f'3 - Вывести текущий баланс\n'
+                          f'4 - Выйти из банкомата\n'
+                          f'5 - Посмотреть Лог операций\n'
+                          f'Вводить сюда ---->: '))
+        my_bank.bank_account, *_ = my_bank.check_bank()
+        if _[0]:
+            print(_[0])
+        result = my_bank.start_bank(my_bank.user_choice(start))
+        if start == 4:
+            print(result)
+            quit()
+        elif start == 1 or start == 2:
+            bank_account = result
+            count, *_ = my_bank.count_checker(my_bank.count)
+            if _:
+                print(_[0])
+        else:
+            print(result)
